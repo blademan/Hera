069323640753e842a1059324f2c9fd6a3f370a04
HeiM <blademan82@gmail.com>
2018-08-17 13:34:30 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+var slideshow=new TINY.slider.slide('slideshow',{
+	id:'slider',
+	auto:4,
+	resume:false,
+	vertical:false,
+	navid:'pagination',
+	activeclass:'current',
+	position:0,
+	rewind:false,
+	elastic:true,
+	left:'slideleft',
+	right:'slideright'
+});
+</script>
